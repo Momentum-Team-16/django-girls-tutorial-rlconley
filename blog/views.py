3e433fcb64de77ec6cde2a57b292f11ab0212133
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2022-12-15 16:37:28 -0500

    working homepage

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    # this goes to the db and retrieves the requested objects
+    # pass these objects as context to the template
+    return render(request, 'blog/post_list.html', {'posts': posts})
